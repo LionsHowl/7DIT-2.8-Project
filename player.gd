4180cf0a4ee8f9414ac34e789fb566c56273b40a
LionsHowl <21kleftley@wakatipu.school.nz>
2024-06-06 18:13:50 +1200

    Coding the animation and added movement

---
+++ player.gd
@@ -0,0 +1,10 @@
+extends CharacterBody2D
+
+func _physics_process(_delta):
+	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
+	velocity = direction * 100
+	move_and_slide()
+	if velocity.length() > 0:
+		get_node("Animated Body").play_Walking_animation()
+	else:
+		get_node("Animated Body").play_Idle_animation()
