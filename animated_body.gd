e2e852074ab1762500b053bacb646555336d8529
LionsHowl <21kleftley@wakatipu.school.nz>
2024-06-18 10:34:58 +1200

    I tidied my files again to make clear space I also made white space consistant between files and changed the radius of the collision shape 2d node.
    I didn't delete the other fire pngs in the folder flame 10 because I was unsure if I wanted to animate the fire spell and decided to put that decision to if I have time in sprint three. And I didn't mention this last time I deleted some files but I kept the attacking, hurt and idle animations because I have plans to use them this sprint.

--- animated_body.gd
+++ animated_body.gd
@@ -1,7 +1,9 @@
 extends AnimatedSprite2D
 
+
 func play_Idle_animation():
 	play("Idle")
+
 
 func play_Walking_animation():
 	play("Walking")
--- player.gd
+++ player.gd
@@ -1,8 +1,11 @@
 extends CharacterBody2D
 
+
 func _physics_process(_delta):
 	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
+	
 	velocity = direction * 100
+	
 	move_and_slide()
 	if velocity.length() > 0:
 		get_node("Animated Body").play_Walking_animation()
