df4c4ae18c6155f6045b077960e6812b03e00ffc
LionsHowl <21kleftley@wakatipu.school.nz>
2024-06-24 19:23:32 +1200

    I've added a health system to the player and entered a way to check that it works by immobilising the player.

--- player.gd
+++ player.gd
@@ -1,13 +1,21 @@
 extends CharacterBody2D
 
+var health = 50
 
-func _physics_process(_delta):
+
+func _physics_process(delta):
 	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
+	var overlaping_weak_mobs = %"Hurt Box/Weak".get_overlapping_bodies()
 	
 	velocity = direction * 150
 	
+	if health <= 0:
+		velocity = direction * 0
 	move_and_slide()
 	if velocity.length() > 0:
 		get_node("Animated Body").play_Walking_animation()
 	else:
 		get_node("Animated Body").play_Idle_animation()
+	
+	if overlaping_weak_mobs.size() > 0:
+		health -= 1 * overlaping_weak_mobs.size() * delta
