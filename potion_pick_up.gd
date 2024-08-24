b03e207043665f69ed9759fbdf48d8dc405616a1
LionsHowl <141293617+LionsHowl@users.noreply.github.com>
2024-08-24 20:13:33 +1200

    I reordered the nodes as I felt Potion Pick-up shouldn't be in Hurt Box.

--- potion_pick_up.gd
+++ potion_pick_up.gd
@@ -4,9 +4,9 @@ var hp
 
 
 func heal():
-	hp = get_parent().get_parent().health
+	hp = get_parent().health
 	if hp + 10 < %"Health Bar".max_value:
 		hp += 10
 	else:
 		hp = %"Health Bar".max_value
-	get_parent().get_parent().health = hp
+	get_parent().health = hp
