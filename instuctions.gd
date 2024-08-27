41708dc350b1b04775508f119cd543a74837c642
LionsHowl <141293617+LionsHowl@users.noreply.github.com>
2024-08-28 01:24:35 +1200

    I updated the instructions.
    I changed the scale of the back in the pause menu as I thought it would be easier to change one node rather than 4 others.

--- instuctions.gd
+++ instuctions.gd
@@ -5,9 +5,40 @@ signal imenu
 
 func _on_menu_button_pressed():
 	visible = false
+	%Main.visible = true
+	%Fire.visible = false
+	%Water.visible = false
+	%Earth.visible = false
+	%Air.visible = false
+	%Score.visible = false
 	
 	imenu.emit()
 
 
 func _on_menu_instructions():
 	visible = true
+
+
+func _on_main_fire_pressed():
+	%Main.visible = not %Main.visible
+	%Fire.visible = not %Fire.visible
+
+
+func _on_fire_water_pressed():
+	%Fire.visible = not %Fire.visible
+	%Water.visible = not %Water.visible
+
+
+func _on_water_earth_pressed():
+	%Water.visible = not %Water.visible
+	%Earth.visible = not %Earth.visible
+
+
+func _on_earth_air_pressed():
+	%Earth.visible = not %Earth.visible
+	%Air.visible = not %Air.visible
+
+
+func _on_air_score_pressed():
+	%Air.visible = not %Air.visible
+	%Score.visible = not %Score.visible
