083eb2d87fcda394df2b58177f0f0e08cfaee13d
LionsHowl <21kleftley@wakatipu.school.nz>
2024-09-01 21:16:45 +1200

    I made it so that the pause menu wouldn't close if the quit confimation screen was visible
    I also updated the comment for that function.

--- pause.gd
+++ pause.gd
@@ -1,13 +1,17 @@
 extends CanvasLayer
 
+var confirmation
+
 
 func _process(_delta):
-	#If P, E, Shift or Space is pressed then if the pause menu is visible then
-	#it no longer becomes visible and the scene tree is no longer paused.
-	#Otherwise if halt from the main game node is no the scene becomes visible
-	#and the scene tree gets paused.
+	#If P, E, Shift or Space is pressed then if the pause menu is visible and
+	#the quit confirmation isn't then it no longer becomes visible and the
+	#scene tree is no longer paused. Otherwise if halt from the main game node
+	#is no the scene becomes visible and the scene tree gets paused.
+	confirmation = get_node("ColorRect/Quit Button/Confirmation")
+	
 	if Input.is_action_just_pressed("pause"):
-		if visible == true:
+		if visible == true and confirmation.visible == false:
 			visible = false
 			get_tree().set_pause(false)
 		elif get_parent().halt == "no":
